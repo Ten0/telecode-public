1e57f9d69e1e2e57e3f9bc925a254146efce134c
Thomas BESSOU <thomas.bessou@telecom-paristech.fr>
2016-09-29 11:40:48 +0200

    Ajout code de départ Ajout séance 2 (tableau cumulatif, ennoncé + solution)

---
+++ seances/tabCumu/11.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#define pb push_back
+#define endl "\n"
+#define PI 3.14159265359
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define TRACE(x) if(DEBUG) cout << #x << " = " << x << endl
+#define IN(v,n) REP(i,n) cin >> v[i];
+#define _ << " " <<
+
+typedef int int_32;
+#define int long long
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+int_32 main() {
+	ios_base::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	vector<int> t(n);
+	IN(t,n);
+
+	vector<int> s(n+1);
+	REP(i,n) s[i+1] = s[i] + t[i];
+
+	int p;
+	cin >> p;
+	REP(ir,p) {
+		int i,j;
+		cin >> i >> j;
+		cout << s[j] - s[i];
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ start/s.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+#define pb push_back
+#define endl "\n"
+#define PI 3.14159265359
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define IN(v,n) REP(i,n) cin >> v[i];
+#define TRACE(x) if(DEBUG) cout << #x << " = " << x << endl
+#define _ << " " <<
+
+typedef int int_32;
+#define int long long
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+int_32 main() {
+	ios_base::sync_with_stdio(0);
+
+	
+
+	return 0;
+}
